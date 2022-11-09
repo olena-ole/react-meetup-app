93e582a8c6c4da032b9fabc77256ea9dcb05cfd4
Olena <pampam32121@gmail.com>
2022-11-09 06:29:24 +0200

    added routes to dummy pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups'
+import NewMeetupsPage from './pages/NewMeetups'
+import FavoitesPage from './pages/Favorites'
+
 function App() {
-  return <div></div>;
+  return (
+   <div>
+    <Route exact path='/'>
+      <AllMeetupsPage />
+    </Route>
+    <Route exact path='/new-meetups'>
+      <NewMeetupsPage />
+    </Route>
+    <Route exact path='/favorites'>
+      <FavoitesPage />
+    </Route>
+   </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Favorites.js
@@ -0,0 +1,3 @@
+export default function FavoitesPage() {
+    return <div>Favorites Page</div>
+}
