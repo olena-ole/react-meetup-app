3bc1a54bb1f0c31832dc9a26c06cf23b439ef250
Olena <pampam32121@gmail.com>
2022-11-09 06:30:43 +0200

    added Switch to routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups'
 import NewMeetupsPage from './pages/NewMeetups'
 import FavoitesPage from './pages/Favorites'
@@ -6,15 +6,17 @@ import FavoitesPage from './pages/Favorites'
 function App() {
   return (
    <div>
-    <Route exact path='/'>
-      <AllMeetupsPage />
-    </Route>
-    <Route exact path='/new-meetups'>
-      <NewMeetupsPage />
-    </Route>
-    <Route exact path='/favorites'>
-      <FavoitesPage />
-    </Route>
+    <Switch>
+      <Route exact path='/'>
+        <AllMeetupsPage />
+      </Route>
+      <Route exact path='/new-meetups'>
+        <NewMeetupsPage />
+      </Route>
+      <Route exact path='/favorites'>
+        <FavoitesPage />
+      </Route>
+    </Switch>
    </div>
   );
 }
