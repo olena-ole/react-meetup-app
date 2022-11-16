a112ccffc05d1a1323f9ff572293ac372bb7dc59
Olena <pampam32121@gmail.com>
2022-11-17 00:12:42 +0200

    mapped through the meetup dummy data in allmeetups component

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,6 +1,34 @@
+const DUMMY_DATA = [
+    {
+      id: 'm1',
+      title: 'This is a first meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
+    {
+      id: 'm2',
+      title: 'This is a second meetup',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+      address: 'Meetupstreet 5, 12345 Meetup City',
+      description:
+        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+    },
 
-
+]
 
 export default function AllMeetupsPage() {
-    return <div>All Meetups Page</div>
+    return (
+        <section>
+            <h1>All Meetups Page</h1>
+            <ul>
+                {DUMMY_DATA.map(item => {
+                    return <li key={item.id}>{item.title}</li>
+                })}
+            </ul>
+        </section>
+    )
 }
