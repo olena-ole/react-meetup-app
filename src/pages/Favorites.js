5c7ad1d1ed0c55f62b5e33034d975dcb257fe6fd
Olena <pampam32121@gmail.com>
2022-11-17 22:30:23 +0200

    favorites page displays items added to favorites using context

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,3 +1,17 @@
+import { useContext } from "react"
+import FavoritesContext from "../store/favorites-context"
+import MeetupList from "../components/meetups/MeetupList"
+
 export default function FavoitesPage() {
-    return <div>Favorites Page</div>
+    const favoritesCtx = useContext(FavoritesContext)
+    const content = favoritesCtx.totalFavorites === 0 ?
+                    <p>You have no favorites yet... Start adding some?</p> : 
+                    <MeetupList meetups={favoritesCtx.favorites}/>
+
+    return (
+        <section>
+            <h1>My Favorites</h1>
+            {content}
+        </section>
+    )
 }
