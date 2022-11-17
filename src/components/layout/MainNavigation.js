2884c3c65a7195dfabb96712baae1ad9b38f41b2
Olena <pampam32121@gmail.com>
2022-11-18 00:46:44 +0200

    added favorites badge displaying favorite meetups count

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,14 +1,19 @@
+import { useContext } from "react"
+import FavoritesContext from "../../store/favorites-context"
 import { Link } from "react-router-dom"
 import classes from './MainNavigation.module.css'
 
 export default function MainNavigation() {
+
+    const favoritesCtx = useContext(FavoritesContext)
+
     return (
         <header className={classes.header}>
             <div className={classes.logo}>React Meetups</div>
             <ul>
                 <li><Link to='/'>All Meetups</Link></li>
-                <li><Link to='/favorites'>Favorites</Link></li>
                 <li><Link to='/new-meetups'>New Meetups</Link></li>
+                <li><Link to='/favorites'>Favorites <span className={classes.badge}>{favoritesCtx.totalFavorites}</span></Link></li>
             </ul>
         </header>
     )
